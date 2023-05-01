21da588ad5a2dd424819977b0cf0e14f4be15448
Alejandro Mascort Colomer <alejandro.mascort17@hotmail.com>
2023-05-01 17:11:39 +0200

    add more specific lsp enter keymap

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -29,8 +29,11 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
 	['<C-m>'] = cmp.mapping.select_next_item(cmp_select),
 	['<C-y>'] = cmp.mapping.confirm({ select = true }),
-	["<C-Space>"] = cmp.mapping.complete(),
-	["<CR>"] = cmp.mapping.confirm({ select = true }),
+	['<C-Space>'] = cmp.mapping.complete(),
+	['<CR>'] = cmp.mapping.confirm({
+		behavior = cmp.ConfirmBehavior.Replace,
+		select = true,
+	}),
 })
 
 cmp_mappings['<Tab>'] = nil
