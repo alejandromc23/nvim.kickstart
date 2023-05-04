63924377e3bfa037133e80c6cd89e3ce8eff0061
Alejandro Mascort Colomer <alejandro.mascort17@hotmail.com>
2023-05-04 19:13:04 +0200

    change autocomplete lsp map to right arrow

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -30,7 +30,7 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 	['<C-m>'] = cmp.mapping.select_next_item(cmp_select),
 	['<C-y>'] = cmp.mapping.confirm({ select = true }),
 	['<C-Space>'] = cmp.mapping.complete(),
-	['<CR>'] = cmp.mapping.confirm({
+	['<Right>'] = cmp.mapping.confirm({
 		behavior = cmp.ConfirmBehavior.Replace,
 		select = true,
 	}),
