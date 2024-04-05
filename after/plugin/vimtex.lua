f2a770a6c75a5c9a86fe84eb15f08dc3e4c16a6d
Alejandro Mascort Colomer <alejandro.mascort17@hotmail.com>
2024-04-05 18:14:16 +0200

    correct vimtex.lua

--- after/plugin/vimtex.lua
+++ after/plugin/vimtex.lua
@@ -1,9 +1,3 @@
-vim.api.nvim_exec([[
-  call deoplete#custom#var('omni', 'input_patterns', {
-    \ 'tex': g:vimtex#re#deoplete
-    \})
-]], false)
-
 -- This is necessary for VimTeX to load properly. The "indent" is optional.
 -- Note that most plugin managers will do this automatically.
 vim.cmd [[filetype plugin indent on]]
@@ -18,7 +12,7 @@ vim.cmd [[syntax enable]]
 vim.g.vimtex_view_method = 'zathura'
 
 -- Or with a generic interface:
-vim.g.vimtex_view_general_viewer = 'zathurad'
+vim.g.vimtex_view_general_viewer = 'zathura'
 vim.g.vimtex_view_general_options = '--unique file:@pdf\\#src:@line@tex'
 
 -- VimTeX uses latexmk as the default compiler backend. If you use it, which is
@@ -26,4 +20,3 @@ vim.g.vimtex_view_general_options = '--unique file:@pdf\\#src:@line@tex'
 -- want another compiler backend, you can change it as follows. The list of
 -- supported backends and further explanation is provided in the documentation,
 -- see ":help vimtex-compiler".
-vim.g.vimtex_compiler_method = 'latexrun'
